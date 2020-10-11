250f7a2a3c9fa370caf1227b55fd42a779168620
Mauricio <mba097@gmail.com>
2020-10-11 18:53:01 -0300

    Mudanças iniciais

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edite <code>src/App.js</code> salve e atualize a paginas.
         </p>
         <a
           className="App-link"
