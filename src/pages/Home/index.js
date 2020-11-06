bed3b044eec12253ad2f7cbcbadb750d0f0b4504
Mauboa <mba097@gmail.com>
2020-11-06 20:57:56 -0300

    Definicao de cores e layout e recuperacao de dados

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,28 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import api from '../../services/api'
 import './styles.css'
+const moment = require('moment')
 
 
 export default function Home() {
+    const [vehicles, setVehicles] = useState([]);
+    const [trips, setTrips] = useState([]);
+    const [schedulings, setSchedulings] = useState([]);
+
     useEffect(() => {
         async function loadAll() {
-            // await api.get('')
+            let resp = await api.get('/trip')
+            console.log(resp.data)
+            setTrips(resp.data)
+
+            resp = await api.get('/scheduling')
+            console.log(resp.data)
+            setSchedulings(resp.data)
+
+            resp = await api.get('/vehicle')
+            console.log(resp.data)
+            setVehicles(resp.data)
+
         }
         loadAll()
     }, [])
@@ -36,33 +52,50 @@ export default function Home() {
                     Agendamento
                 </strong>
                 <ul className="list-agendamentos">
-                    <li>
-                        <div className="dia-especifico">
-                            <label>
-                                Dia especifico: DD/MM/YYYY
-                            </label>
-                        </div>
-                        <div className="hora-agendamento">
-                            HI:HI - HF:HF
-                        </div>
-                    </li>
-                    <li>
-                        <div className="dia-da-semana">
-                            <div>Seg</div>
-                            <div>Ter</div>
-                            <div>Qua</div>
-                            <div>Qui</div>
-                            <div>Sex</div>
-                            <div>Sab</div>
-                            <div>Dom</div>
-                        </div>
-                        <div className="hora-agendamento">
-                            HI:HI - HF:HF
-                        </div>
-                        <div className="dia-agendamento">
-                            DD/MM/YYYY - DD/MM/YYYY
-                        </div>
-                    </li>
+                    {schedulings.map(scheduling => {
+                        var dias = ''
+                        var intevalos = ''
+                        var dataEspecifica = moment(scheduling.specificDay, "YYYY-MM-DD").format('DD-MM-YY')
+
+                        //Dia especifico ou dias da semana 
+                        if (scheduling.weekDays == null) {
+                            // if (scheduling.specificDay) {
+                            dias = <div className="dia-especifico">
+                                <label>
+                                    Dia especifico: {dataEspecifica}
+                                </label>
+                            </div>
+                            // }
+                        } else {
+                            dias = <div className="dia-da-semana">
+                                <div>Seg</div>
+                                <div>Ter</div>
+                                <div>Qua</div>
+                                <div>Qui</div>
+                                <div>Sex</div>
+                                <div>Sab</div>
+                                <div>Dom</div>
+                            </div>
+                        }
+
+                        //Intervalo de dias
+                        if (scheduling.startDay) {
+                            intevalos =
+                                <div className="dia-agendamento">
+                                    {moment(scheduling.startDay, "YYYY-MM-DD").format('DD-MM-YY')} - {moment(scheduling.endDay, "YYYY-MM-DD").format('DD-MM-YY')}
+                                </div>
+                        }
+                        return (
+                            <li key={scheduling.id}>
+                                {dias}
+                                <div className="hora-agendamento">
+                                    {scheduling.startTime.substr(0, 5)} - {scheduling.endTime.substr(0, 5)}
+                                </div>
+                                {intevalos}
+                            </li>
+                        )
+                    }
+                    )}
                 </ul>
             </div>
             <div className="veiculos">
@@ -70,17 +103,27 @@ export default function Home() {
                     Veiculos
                 </strong>
                 <ul className="list-veiculos">
-                <li>
-                        <header>
-                            Veiculo
-                        </header>
-                        <label>
-                            Descricao:
-                        </label>
-                        <span>
-                            Capacidade: 05/15
-                        </span>
-                    </li>
+                    {vehicles.map(vehicle => (
+                        <li key={vehicle.id}>
+                            <header>
+                                <label>
+                                    {vehicle.name}
+                                </label>
+                                <div>
+                                    <a>O</a>
+                                    <a>X</a>
+                                </div>
+                            </header>
+                            <label>
+                                Descricao:
+                                {vehicle.description}
+                            </label>
+                            <div>
+                                Capacidade: {vehicle.capacity}
+                            </div>
+                        </li>
+                    ))}
+
                 </ul>
             </div>
             <div className="proximos">
